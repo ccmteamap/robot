8787476a7d1f8c74935fdf169c231a24f3ae042c
Ruven Salamon <salamon.ruven@gmail.com>
2017-04-01 01:44:45 +0200

    kortste afstand wordt geselecteerd. waarde die uit versnellingsfunctie komt wordt omgezet naar int

--- DRIVING/src/main/src/driving.cpp
+++ DRIVING/src/main/src/driving.cpp
@@ -3,9 +3,28 @@
 
 void drive(bool forward){
   extern Motor mainMotor;
+  extern ExpoAvrg ultraPAvrg;
+  extern ExpoAvrg ultraEAvrg;
+
+  double x = ultraPAvrg <= ultraEAvrg ? ultraPAvrg : ultraEAvrg;
+  double y = function(x);
+
+  int speed = (int)(y + 0.5);
+
+  if(!forward){
+    speed *= -1;
+  }
+
+  mainMotor.SetSpeed(speed);
 }
 
 void stopDriving(){
   extern Motor mainMotor;
   mainMotor.SetSpeed(0);
 }
+
+void function(double x){
+
+  return y;
+}
+
